56a581bb4a9c1ff0060b854c01276cdecbede1b4
Keerthana K B <144669335+keerthana-k-b@users.noreply.github.com>
2024-09-07 12:02:56 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.mybutton;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+EditText et;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+        et=(EditText) findViewById(R.id.et);
+    }
+
+    public void name(View view) {
+        et.setText("hai");
+    }
+}
